f0556327995e258b6ca9b30102cc9828ad6c67ca
verluxstands <marketing.verlux@gmail.com>
2025-09-30 18:01:25 +0530

    add frontend endpoint

--- server/cors.ts
+++ server/cors.ts
@@ -1,6 +1,7 @@
 import cors from 'cors';
 
 const allowedOrigins = [
+  'https://tech2saini.vercel.app/',
   'http://localhost:5173', // Vite default
   'http://localhost:5174/',
   'http://localhost:3000', // Common React dev port
